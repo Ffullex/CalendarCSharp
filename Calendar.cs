2b9dda6d4c2388ad73edbf0d5b22f30da11f849e
Yar Ffullex <72185613+Ffullex@users.noreply.github.com>
2021-01-20 23:25:44 +0700

    The calendar

---
+++ Calendar.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Calendar
+{
+    public class Calendar
+    {
+        public DateTime actualTime = DateTime.Now;
+
+        public void DateShift(TimeSpan timeSpan)
+        {
+            actualTime += timeSpan;
+        }
+
+        public void ConsoleCalendar()
+        {
+            DateTime weekDay;
+            bool firstIteration = true;
+
+            Console.WriteLine($"{actualTime.ToString("MMMMMMMM yyy")}\nПн\tВт\tСр\tЧт\tПт\tСб\tВс");
+
+            for (int day = 1; day <= DateTime.DaysInMonth(actualTime.Year, actualTime.Month); ++day)
+            {
+                weekDay = new DateTime(actualTime.Year, actualTime.Month, day);
+
+                if (firstIteration)
+                {
+                    if (weekDay.DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        for (int firstDay = 5; firstDay >= 0; --firstDay)
+                        {
+                            Console.Write("\t");
+                        }
+                    }
+                    else
+                    {
+                        for (int firstDay = (int)weekDay.DayOfWeek - 1; firstDay > 0; --firstDay)
+                        {
+                            Console.Write("\t");
+                        }
+                    }
+                    firstIteration = false;
+                }
+                /*
+                 * Описатель для полного названия месяца MMMM
+                 * 
+                 */
+
+                {
+                    if (weekDay.Day == actualTime.Day)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.Write((int)weekDay.Day + "\t");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write((int)weekDay.Day + "\t");
+                    }
+
+
+                    if (weekDay.DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        Console.WriteLine("\n");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calendar.HappyNewYearTheme
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Calendar calendar = new Calendar();
+            calendar.ConsoleCalendar();
+
+
+
+            List<TimeSpan> timeSpans = new List<TimeSpan>
+            {
+                new TimeSpan(6, 7, 2, 3),
+                new TimeSpan(8, 5, 6, 9)
+
+            };
+
+            foreach (TimeSpan time in timeSpans)
+            {
+                Console.WriteLine("\n");
+                calendar.DateShift(time);
+                calendar.ConsoleCalendar();
+
+                Console.WriteLine();
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
